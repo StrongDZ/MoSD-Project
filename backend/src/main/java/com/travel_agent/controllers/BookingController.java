ca4ad17b7f38a3295ed12657ae8ec01bf71e183c
hmcuone <cuong_hm@amira.vn>
2025-10-15 10:21:00 +0700

    feat: add service methods to get hotel and ship bookings by ID

--- backend/src/main/java/com/travel_agent/controllers/BookingController.java
+++ backend/src/main/java/com/travel_agent/controllers/BookingController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/booking")
 @RequiredArgsConstructor
+
 public class BookingController {
     private final BookingService bookingService;
 
