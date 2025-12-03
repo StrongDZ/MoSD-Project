ac22ba40740f0d4e84e819a600d6a9d5ea33d7fe
{Dangg} <shadowquapro@gmail.com>
2025-12-03 11:02:12 +0700

    feat: add Account and User DTOs

---
+++ backend/src/main/java/com/travel_agent/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.travel_agent.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Getter
+@Setter
+@Data
+@EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = false)
+public class UserDTO extends AccountDTO{
+    private LocalDate dob;
+}
