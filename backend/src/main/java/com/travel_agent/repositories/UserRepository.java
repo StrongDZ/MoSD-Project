9890435137eb4b1198df56251b4b08187ebc12c3
{Dangg} <shadowquapro@gmail.com>
2025-12-03 11:04:55 +0700

    feat: create UserRepository interface

---
+++ backend/src/main/java/com/travel_agent/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.travel_agent.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.travel_agent.models.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByUsernameAndPassword(String username, String password);
+}
