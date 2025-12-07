dc76a95c150d8aaea5a9ea194431079475c76c46
{Dangg} <shadowquapro@gmail.com>
2025-12-07 17:01:37 +0700

    feat(jwt) : add complete jwt feat

--- backend/src/main/java/com/travel_agent/utils/JwtUtils.java
+++ backend/src/main/java/com/travel_agent/utils/JwtUtils.java
@@ -31,3 +31,71 @@ public class JwtUtils {
         return extractClaim(token, Claims::getSubject);
     }
  
+    public Date extractExpiration(String token) {                                            
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public String extractRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
+    }
+    public String extractPassword(String token) {
+        return extractClaim(token, claims -> claims.get("password", String.class));
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(String username,  String role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        return new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public Authentication getAuthentication(String token) {
+        String username = extractUsername(token);
+        String role = extractRole(token);
+        String password = extractPassword(token);   
+
+        role = "ROLE_" + role.toUpperCase();
+
+        // Tạo ra 1 Authentication object có username, password và quyền
+        return new UsernamePasswordAuthenticationToken(
+                username,
+                password,
+                Collections.singletonList(new SimpleGrantedAuthority(role))
+        );
+    }
+}
